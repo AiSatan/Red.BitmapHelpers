bab05d124919915850ec41474106a66285e9d073
Fandus Atipov <aisatan.ne@live.com>
2017-03-29 01:02:41 +0300

    added test

---
+++ Red.BitmapHelper.Test/BitmapTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Red.BitmapHelpers;
+
+namespace Red.BitmapHelper.Test
+{
+    [TestClass]
+    public class BitmapTest
+    {
+        [TestMethod]
+        public void LoadBmp()
+        {
+            var bmp = RedHelper.LoadBitmap("Test.bmp");
+            Assert.IsNotNull(bmp);
+        }
+
+        [TestMethod]
+        public void GetScreen()
+        {
+            var bmp = RedHelper.GetCopyFromScreen(0, 0, 2, 2);
+            Assert.IsNotNull(bmp);
+        }
+
+        [TestMethod]
+        public void GetColor()
+        {
+            var bmp = RedHelper.LoadBitmap("Test.bmp");
+            var pixels = bmp.BitmapToByteRgbQ();
+            var bmpWidth = bmp.Width;
+            var bmpHeight = bmp.Height;
+            for (var x = 0; x < bmpWidth; x++)
+            {
+                for (var y = 0; y < bmpHeight; y++)
+                {
+                    var color = pixels.GetColor(x, y);
+                    Assert.IsTrue(color.R == Color.Red.R);
+                }
+            }
+        }
+    }
+}
---
+++ Red.BitmapHelper.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Red.BitmapHelper.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Red.BitmapHelper.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97e67243-e003-4745-95a9-f04477d0a292")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
